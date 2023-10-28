19f54c27f0c9a809dc37d00a1aa6fe04d6248d89
Reseraju <reseraju21@gmail.com>
2023-10-28 21:33:59 +0530

    updated

--- includes/config.php
+++ includes/config.php
@@ -1,8 +1,8 @@
 <?php
-define('DB_SERVER','sql201.infinityfree.com');
-define('DB_USER','	if0_35322099');
-define('DB_PASS' ,'gaBcP2pEoSL');
-define('DB_NAME', 'if0_35322099_ems');
+define('DB_SERVER','localhost');
+define('DB_USER','root');
+define('DB_PASS' ,'');
+define('DB_NAME', 'ems');
 $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 // Check connection
 if (mysqli_connect_errno())
